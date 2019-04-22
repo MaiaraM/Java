744648bca381305aeb56ec785acfbade0d448c40
MaiaraM <maiaramartins093@gmail.com>
2019-04-22 16:50:37 -0300

    Inicial

---
+++ Hibernate/src/test/java/TesteConta.java
@@ -0,0 +1,27 @@
+import br.com.galaxyware.financas.modelo.Conta;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.swing.text.html.parser.Entity;
+
+public class TesteConta {
+
+    public static void main(String[] args) {
+        Conta conta = new Conta();
+        conta.setTitular("Leonardo");
+        conta.setBanco("Caixa Economica");
+        conta.setAgencia("123");
+        conta.setNumero("456");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("financas");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        em.persist(conta);
+        em.getTransaction().commit();
+
+        em.close();
+        emf.close();
+    }
+}
